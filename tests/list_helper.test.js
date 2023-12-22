e9f5945e266e41f973ef557ebffb5d06b6a315ab
Lucas Amberg <102396588+lucas-amberg@users.noreply.github.com>
2023-12-21 19:03:41 -0800

    Create favoriteBlog function and run tests (4.5)

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -58,3 +58,59 @@ describe('total likes', () => {
     })
 })
 
+describe('favorite blog', () => {
+    const listWithOneBlog = [
+        {
+          _id: '5a422aa71b54a676234d17f8',
+          title: 'Go To Statement Considered Harmful',
+          author: 'Edsger W. Dijkstra',
+          url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+          likes: 5,
+          __v: 0
+        }
+      ]
+
+    const listWithNoBlogs = []
+
+    const listWithTwoBlogs = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: 'd228dhjhd82hdiajsd',
+            title: 'Lucas Amberg Github',
+            author: 'Lucas Amberg',
+            url: 'https://www.github.com/lucas-amberg',
+            likes: 23,
+            __v: 0
+        }
+    ]
+
+    test('when there is no blogs returns \"None\"', () => {
+        const result = listHelper.favoriteBlog(listWithNoBlogs)
+        expect(result).toBe("None")
+    })
+    test('when there is only one blog returns self', () => {
+        const result = listHelper.favoriteBlog(listWithOneBlog)
+        expect(result).toEqual({
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            likes: 5
+        })
+    })    
+
+    test('when there is multiple blogs returns the blog with the most likes', () => {
+        const result = listHelper.favoriteBlog(listWithTwoBlogs)
+        expect(result).toEqual({
+            title: 'Lucas Amberg Github',
+            author: 'Lucas Amberg',
+            likes: 23
+        })
+    })
+})
+
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -12,7 +12,19 @@ const totalLikes = (blogs) => {
     : blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+        return "None"
+    }
+    const blogWithMostLikes = blogs.reduce((mostLiked, current) => {
+        return current.likes > mostLiked.likes ? current : mostLiked
+    }, blogs[0])
+    const {__v, _id, url, ...remainingObject} = blogWithMostLikes
+    return remainingObject
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
