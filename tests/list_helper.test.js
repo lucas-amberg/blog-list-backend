3c26e520d7dc923228efc51509522b461465f4f2
Lucas Amberg <102396588+lucas-amberg@users.noreply.github.com>
2023-12-21 18:41:44 -0800

    Create totalLikes function and run tests (4.4)

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,8 +1,60 @@
 const listHelper = require('../utils/list_helper')
 
-test('dummy returns one', () => {
-    const blogs = []
-
-    const result = listHelper.dummy(blogs)
-    expect(result).toBe(1)
+describe('dummy', () => {
+    test('dummy returns one', () => {
+        const blogs = []
+    
+        const result = listHelper.dummy(blogs)
+        expect(result).toBe(1)
+    })
 })
+
+describe('total likes', () => {
+    const listWithOneBlog = [
+        {
+          _id: '5a422aa71b54a676234d17f8',
+          title: 'Go To Statement Considered Harmful',
+          author: 'Edsger W. Dijkstra',
+          url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+          likes: 5,
+          __v: 0
+        }
+      ]
+
+    const listWithNoBlogs = []
+
+    const listWithTwoBlogs = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: 'd228dhjhd82hdiajsd',
+            title: 'Lucas Amberg Github',
+            author: 'Lucas Amberg',
+            url: 'https://www.github.com/lucas-amberg',
+            likes: 23,
+            __v: 0
+        }
+    ]
+
+    test('when list has no blogs, equals zero', () => {
+        const result = listHelper.totalLikes(listWithNoBlogs)
+        expect(result).toBe(0)
+    })
+
+    test('when list has only one blog, equals the likes of that', () => {
+        const result = listHelper.totalLikes(listWithOneBlog)
+        expect(result).toBe(5)
+    })
+
+    test('when list has multiple blogs, equals the sum of the likes of those blogs', () => {
+        const result = listHelper.totalLikes(listWithTwoBlogs)
+        expect(result).toBe(28)
+    })
+})
+
