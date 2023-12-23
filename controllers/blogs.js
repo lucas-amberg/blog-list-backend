dc68fda06b46dde7a7071754b2e0044473a1f05c
Lucas Amberg <102396588+lucas-amberg@users.noreply.github.com>
2023-12-23 12:31:54 -0800

    Expand blogs to add info on blog creator (4.17)

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const logger = require('../utils/logger')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -9,7 +10,7 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
 
   if (!blog.likes) {
@@ -21,14 +22,17 @@ blogsRouter.post('/', (request, response) => {
   }
 
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-    .catch((error) => {
-      logger.error('error posting to API: ', error.message)
-    })
+
+  const user = await User.findById('6587426dd7e3f39ad00b7f63')
+  
+  blog.user = user._id
+   
+
+
+  const result = await blog.save()
+  user.blogs = user.blogs.concat(result._id)
+  await user.save()
+  response.status(201).json(result)
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
